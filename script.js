ee1752f6827505c36c6708871eeb4495301cce58
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-12 20:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+setTimeout(()=>{
+	console.log("macrotask")
+},1000)
+const preomise = new Promise((resolve,reject)=>{
+	resolve("i am in microtask")
+})
+preomise.then((data)=>{
+	console.log("resolve" + data)
+})
